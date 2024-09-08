2411eb2e2890f29474324d3ca0fa0556074c6ab6
jamor <jamor.devbr@gmail.com>
2024-09-07 22:39:39 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,61 @@
+function pesquisar(){
+  let sector = document.getElementById("resultPesquisa");
+  
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  let result = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+  
+  if (campoPesquisa == ""){
+    sector.innerHTML = `
+    <div class="item-resultado">            
+      <h2>Busca vazia!</h2>          
+      <p class="descricao-meta">Digite sua pesquisa novamente no campo acima.</p>
+    </div>
+    `;
+    return
+  }  
+
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){    
+      result += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>          
+          <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+        </div>
+      `;
+    }
+  }
+/*
+  if(result == false){    
+    result = "<div class="item-resultado"><h2>Desculpa, não encontramos o que procura.</h2>  <p>Tente uma nova pesquisa</p></div>"
+  }*/
+
+
+  sector.innerHTML = result;
+ 
+//**Essa foi a minha solução inicialmente**
+
+  if(!result){    
+    sector.innerHTML = `
+    <div class="item-resultado">
+        <h2>
+          Desculpa, não encontramos o que procura.
+        </h2> 
+        <p class="descricao-meta">Digite uma nova pesquisa no campo acima.</p>   
+      </div>   
+    `;
+    return
+  }
+}
+ 
---
+++ dados.js
@@ -0,0 +1,46 @@
+
+let dados = [
+    {
+        titulo: "House of Dragon",
+        descricao: "Série se passa cerca de 200 anos antes dos eventos de Game of Thrones e narra a Guerra dos Trono, um conflito sangrento entre os membros da Casa Targaryen pela sucessão ao Trono de Ferro.",
+        link: "https://pt.wikipedia.org/wiki/House_of_the_Dragon",
+        tags: "Fantasia, Casa Targaryen, Game of Thrones, Dragão, Trono, Guerra"
+    },
+    {
+        titulo: "Stranger Things",
+        descricao: "Série de ficção científica ambientada nos anos 80, que acompanha um grupo de amigos que investigam o desaparecimento de um de seus membros e eventos sobrenaturais em sua pequena cidade.",
+        link: "https://pt.wikipedia.org/wiki/Stranger Things",
+        tags: "Ficção Científica, Anos 80, Mistério, Sobrenatural"
+    },
+    {
+        titulo: "Game of Thrones",
+        descricao: "Adaptação da saga de livros As Crônicas de Gelo e Fogo, a série narra a luta por poder entre as grandes famílias de Westeros pelo Trono de Ferro.",
+        link: "https://pt.wikipedia.org/wiki/Game_of Thrones",
+        tags: "Fantasia, Medieval, Dragões, Política, Guerra"
+    },
+    {
+        titulo: "Rick e Morty",
+        descricao: "Série de animação para adultos que acompanha as aventuras interdimensionais do cientista louco Rick Sanchez e seu neto Morty Smith.",
+        link: "https://pt.wikipedia.org/wiki/Rick_e_Morty",
+        tags: "Animação, Ciencia Ficção, Comédia, Viagens no Tempo"
+    },
+    {
+        titulo: "The Witcher",
+        descricao: "Série de fantasia baseada nos livros de Andrzej Sapkowski, que acompanha um caçador de monstros solitário em um mundo medieval.",
+        link: "https://pt.wikipedia.org/wiki/The_Witcher",
+        tags: "Fantasia, Medieval, Monstros, Magia"
+    },
+    {
+        titulo: "Black Mirror",
+        descricao: "Antologia que explora as consequências da tecnologia em nossa sociedade, apresentando histórias independentes com temas como realidade virtual, inteligência artificial e redes sociais.",
+        link: "https://pt.wikipedia.org/wiki/Black_Mirror",
+        tags: "Ficção Científica, Tecnologia, Distopia, Antologia"
+    },
+    {
+        titulo: "Breaking Bad",
+        descricao: "Série que acompanha a transformação de um professor de química em um poderoso traficante de drogas.",
+        link: "https://pt.wikipedia.org/wiki/Breaking_Bad",
+        tags: "Drama, Crime, Drogas"
+    }
+];
+
