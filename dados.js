8953d5125701cc26d91e0aaaf37e07f7a6249cb3
jamor <jamor.devbr@gmail.com>
2024-09-07 23:05:42 -0300

    First Commit2

--- dados.js
+++ dados.js
@@ -4,43 +4,43 @@ let dados = [
         titulo: "House of Dragon",
         descricao: "Série se passa cerca de 200 anos antes dos eventos de Game of Thrones e narra a Guerra dos Trono, um conflito sangrento entre os membros da Casa Targaryen pela sucessão ao Trono de Ferro.",
         link: "https://pt.wikipedia.org/wiki/House_of_the_Dragon",
-        tags: "Fantasia, Casa Targaryen, Game of Thrones, Dragão, Trono, Guerra"
+        tags: "Fantasia, Casa Targaryen, Game of Thrones, Dragão, Trono, Guerra, all"
     },
     {
         titulo: "Stranger Things",
         descricao: "Série de ficção científica ambientada nos anos 80, que acompanha um grupo de amigos que investigam o desaparecimento de um de seus membros e eventos sobrenaturais em sua pequena cidade.",
         link: "https://pt.wikipedia.org/wiki/Stranger Things",
-        tags: "Ficção Científica, Anos 80, Mistério, Sobrenatural"
+        tags: "Ficção Científica, Anos 80, Mistério, Sobrenatural, all"
     },
     {
         titulo: "Game of Thrones",
         descricao: "Adaptação da saga de livros As Crônicas de Gelo e Fogo, a série narra a luta por poder entre as grandes famílias de Westeros pelo Trono de Ferro.",
         link: "https://pt.wikipedia.org/wiki/Game_of Thrones",
-        tags: "Fantasia, Medieval, Dragões, Política, Guerra"
+        tags: "Fantasia, Medieval, Dragões, Política, Guerra, all"
     },
     {
         titulo: "Rick e Morty",
         descricao: "Série de animação para adultos que acompanha as aventuras interdimensionais do cientista louco Rick Sanchez e seu neto Morty Smith.",
         link: "https://pt.wikipedia.org/wiki/Rick_e_Morty",
-        tags: "Animação, Ciencia Ficção, Comédia, Viagens no Tempo"
+        tags: "Animação, Ciencia Ficção, Comédia, Viagens no Tempo, all"
     },
     {
         titulo: "The Witcher",
         descricao: "Série de fantasia baseada nos livros de Andrzej Sapkowski, que acompanha um caçador de monstros solitário em um mundo medieval.",
         link: "https://pt.wikipedia.org/wiki/The_Witcher",
-        tags: "Fantasia, Medieval, Monstros, Magia"
+        tags: "Fantasia, Medieval, Monstros, Magia, all"
     },
     {
         titulo: "Black Mirror",
         descricao: "Antologia que explora as consequências da tecnologia em nossa sociedade, apresentando histórias independentes com temas como realidade virtual, inteligência artificial e redes sociais.",
         link: "https://pt.wikipedia.org/wiki/Black_Mirror",
-        tags: "Ficção Científica, Tecnologia, Distopia, Antologia"
+        tags: "Ficção Científica, Tecnologia, Distopia, Antologia, all"
     },
     {
         titulo: "Breaking Bad",
-        descricao: "Série que acompanha a transformação de um professor de química em um poderoso traficante de drogas.",
+        descricao: "Série que acompanha um professor de química que após descobrir um cancer se transforma em um poderoso traficante de drogas.",
         link: "https://pt.wikipedia.org/wiki/Breaking_Bad",
-        tags: "Drama, Crime, Drogas"
+        tags: "Drama, Crime, Drogas, all"
     }
 ];
 
